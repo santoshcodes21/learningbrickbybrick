3fa67f4d9935e92e7084f393de7f577c68a53b68
santoshcodes21 <117723164+santoshcodes21@users.noreply.github.com>
2025-01-03 19:38:43 +0000

    conversions

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,28 @@
+//let score="33abc"
+
+//console.log(typeof score)
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber) //number
+//console.log(valueInNumber)  //NaN
+
+ let score=null
+
+ let valueInNumber=Number(score)
+ console.log(valueInNumber) //0
+
+ // "33" =>33
+ //"33abc"=>NaN(not a number)
+ //true=>1, false=>0
+ //null=>0
+ //undefined=>NaN
+
+ let isLoggedIn=1
+ let booleanLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanLoggedIn)
+ //""=>false
+ //"sky"=>true
+
+ let x=33
+ let stringNumber=String(x)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
