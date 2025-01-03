4b5374151d1c548432f1a2b590512febaf98bccf
santoshcodes21 <117723164+santoshcodes21@users.noreply.github.com>
2025-01-03 18:59:35 +0000

    dataTypes

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,14 @@
+let name="santosh"
+let age =23
+let isLoggedIn=false
+
+//number 2 to power 53
+//bigint
+//string
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+//object
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
