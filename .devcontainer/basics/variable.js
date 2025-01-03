1f09750821590615dd2bd3bf909dd3d2888b7c53
santoshcodes21 <117723164+santoshcodes21@users.noreply.github.com>
2025-01-03 18:28:40 +0000

    variable learning

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=144568
+let accountEmail="skyadav21660@gmail.com"
+var accountPassword="23456"
+accountCity="Kolkata"
+let accountState
+//if we are not assigining value then javascipt will consider it as undefined
+//accountId=234 const value can't be changed
+
+accountEmail="santosh.kumar.csbs25@heritageit.edu.in"
+accountPassword="2894"
+accountCity="Pune"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/* prefer not to use var becz of issue in block scope and functional scope*/ 
