e2c0fb20b14956ddc5ea4b619cead9a192a4dfc8
santoshcodes21 <117723164+santoshcodes21@users.noreply.github.com>
2025-08-14 07:47:49 +0000

    done ith scope and function in js

---
+++ .devcontainer/03_basics/function.js
@@ -0,0 +1,78 @@
+// console.log("S")
+// console.log("A")
+// console.log("N")
+// console.log("T")
+// console.log("O")
+// console.log("S")
+// console.log("H")
+
+// function sayMyName(){
+//         console.log("S")
+//         console.log("A")
+//         console.log("N")
+//         console.log("T")
+//         console.log("O")
+//         console.log("S")
+//         console.log("H")
+// }
+ 
+//      sayMyName()
+
+// function addTwoNumber(num1,num2){
+//     console.log(num1+num2)
+// }
+
+//   addTwoNumber(5,6)
+
+//   function addTwoNumber(num1,num2){
+//      let res=num1+num2
+//        return res
+//    // return num1+num2
+//   }
+//   const result=addTwoNumber(6,8)
+//   console.log("Result: "+ result) //14
+  
+// function loginUserMessage(userName){
+//     return `${userName} just logged In`
+// }
+// console.log(loginUserMessage("Santosh"))
+
+// function loginUserMessage(userName){
+//     if(!userName){
+//         console.log("Please enter a name")
+//         return
+//     }
+        
+//      return `${userName} just logged In`
+// }
+
+//  console.log(loginUserMessage("Hitesh"))  //Hitesh just logged in
+
+// function calculatePrice(num1){
+//     return num1
+// }
+// console.log(calculatePrice(4)) // 4
+
+// function calculatePrice(...num1){
+//     return num1
+// }
+// console.log(calculatePrice(200,300,400,500))  //[200,300,400,500]
+
+// const user={
+//     username:"hitesh",
+//     price:208
+// }
+
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+// }
+
+// handleObject(user)   // UserName is hitesh and price is 208
+
+// const myNewArray=[200,300,400,500]
+
+// function returnSecondValue(getArray){
+//     return getArray
+// }
+// console.log(returnSecondValue(myNewArray))
+
---
+++ .devcontainer/03_basics/scope.js
@@ -0,0 +1,53 @@
+// let a=10
+//  const b=20
+//  var c=30
+
+//  console.log(a)
+//  console.log(b)
+//  console.log(c)
+
+ {
+    let a=10
+    const b=20
+    var c=30
+ }
+
+// console.log(a)
+// console.log(b)
+// console.log(c) //30
+
+//  function one(){
+//     const userName="hitesh"
+
+
+// function two(){
+//     const website="youtube"
+//     console.log(userName)  //hitesh
+// }
+// //  console.log(website)  //website is out of scope
+//   two()
+// }
+// one()
+
+// if(true){
+//     const userName="Hitesh"
+
+//       if(userName==="Hitesh"){
+//         const website="Youtube"
+//         console.log(userName + website) //HiteshYoutube
+//       }
+//      // console.log(website)  // website is out of scope
+// }
+// //console.log(userName)  //  userName is out of scope
+// console.log(addOne(5))  //6
+// function addOne(num){
+//     return num+1
+// }
+// console.log(addOne(5)) //6
+
+// //console.log(addTwo(8))//can't access before initialization
+
+//  const addTwo=function(num){
+//     return num+2
+// }
+// console.log(addTwo(8))  //10
